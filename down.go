1f77f29092dd6330c98a2959d75f72b5e6ae8288
Sam Dodrill <xena@yolo-swag.com>
2014-11-18 20:41:20 -0800

    Start go rewrite: add down command

---
+++ down.go
@@ -0,0 +1,33 @@
+package main
+
+import (
+	"fmt"
+	"log"
+
+	"github.com/fsouza/go-dockerclient"
+)
+
+func downCommand() {
+	man, err := loadManifest(*manifest)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	_ = man
+
+	c, err := newDockerClient()
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	err = c.RemoveContainer(docker.RemoveContainerOptions{
+		ID:    man.Projname + "-dev",
+		Force: true,
+	})
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fmt.Println("Container destroyed")
+}
